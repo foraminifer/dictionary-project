fe10aa597636dc8758349564217a7680e984a1ff
foraminifer <87345841+foraminifer@users.noreply.github.com>
2022-04-09 17:32:50 -0500

    Added photo api from Pexels

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     console.log(response.data[0]);
     setResults(response.data[0]);
   }
@@ -15,10 +17,20 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
 
+    function handlePexelsResponse(response) {
+      setPhotos(response.data.photos);
+    }
+
     //documentation: https://dictionaryapi.dev/
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "563492ad6f91700001000001319b0d0fcf5342f58c6eda2b94962bd7";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -29,6 +41,7 @@ export default function Dictionary() {
         <input type="search" onChange={handleKeywordChange} />
       </form>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
